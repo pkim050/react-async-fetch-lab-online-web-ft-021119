8dd0e3163594f076c197331a925b9df15e9cdb11
Patrick Kim <pkim050@gmail.com>
2019-07-24 08:04:43 +0000

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,28 @@
 // create your App component here
+import React from 'react';
+
+class App extends React.Component {
+  state = {
+    // creating and inputting data on the new variable will cause same name as existing route error.
+  }
+
+  componentDidMount() {
+    fetch('http://api.open-notify.org/astros.json')
+    .then(resp => resp.json())
+    .then(json => {
+      this.setState({
+        data: json
+      })
+    })
+  }
+
+  render() {
+    return(
+      <div>
+
+      </div>
+    )
+  }
+}
+
+export default App
