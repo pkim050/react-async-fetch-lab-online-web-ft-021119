fa3010f26376acaced15c2b423f57a920b90e335
Patrick Kim <pkim050@gmail.com>
2019-07-24 08:02:41 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -9,10 +9,9 @@ class App extends React.Component {
   componentDidMount() {
     fetch('http://api.open-notify.org/astros.json')
     .then(resp => resp.json())
-    .then(data => {
-      console.log(data)
+    .then(json => {
       this.setState({
-        peopleInSpace: data
+        data: json
       })
     })
   }
@@ -20,7 +19,7 @@ class App extends React.Component {
   render() {
     return(
       <div>
-        {this.state.peopleInSpace.map(person => person.name)}
+        
       </div>
     )
   }
