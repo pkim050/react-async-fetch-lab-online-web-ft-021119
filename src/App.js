4155d24a2f8c00cdbf81fe02e1854cbf903ec66b
Patrick Kim <pkim050@gmail.com>
2019-07-24 08:02:47 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 class App extends React.Component {
   state = {
-    peopleInSpace: []
+
   }
 
   componentDidMount() {
@@ -19,7 +19,7 @@ class App extends React.Component {
   render() {
     return(
       <div>
-        
+
       </div>
     )
   }
